3e749a42ed1840aede45b499ac73a2433e4043b8
AdamEGorka <45395187+AdamEGorka@users.noreply.github.com>
2018-11-29 10:09:33 -0500

    First Method
    Added convertToArrayList method

--- DayTwoGitHub.java
+++ DayTwoGitHub.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
 public class DayTwoGitHub{
+	public static ArrayList<Integer> convertToArrayList(int value){
+		ArrayList<Integer> list = new ArrayList<>();
+		while(value > 0){
+			list.add(0, value%10);
+			value= value/10;
+		}
+		return list;
+	}
 	public static void main(String[]args){
-	//insert stuff here
+		ArrayList<Integer> list = convertToArrayList(8950);
+		System.out.println(list);
+
 	}
 }
