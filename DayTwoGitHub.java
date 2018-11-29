d798d122f1de4cdc68737b84e57febe86c1973ca
Krissy <45279097+krissypins@users.noreply.github.com>
2018-11-29 10:07:07 -0500

    Adding a Method
    adds the method that takes all the numbers from an array list and turns them into one large (squished together) number

--- DayTwoGitHub.java
+++ DayTwoGitHub.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
 public class DayTwoGitHub{
 	public static void main(String[]args){
-	//insert stuff here
+
 	}
+	public static int putTogether(ArrayList<Integer> arr){
+		int x = 0;
+		for(int i = 0; i < arr.size(); i++){
+			x += arr.get(i);
+		}//end for
+		return x;
+	}//close method
 }
